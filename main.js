4b47b7f824e1fbd9893048bf23972e479f305929
Friscxion <erwann.champion@outlook.fr>
2021-08-24 11:01:52 +0200

    Commit

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const schedule = require('node-schedule');
 
+const j = schedule.scheduleJob({hour: 0, minute: 8,name:"ouvrir"}, () => {
+    console.log('Job runs every day at 5:30AM');
+});
 
+console.log(schedule.scheduledJobs)
 app.use('/',require('./routes'))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
